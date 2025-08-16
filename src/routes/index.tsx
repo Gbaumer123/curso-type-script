c885ba62b1642952782d73444cc3680e7a7c7e3b
Gabriel Baumer <gbaumer123@gmail.com>
2025-08-15 22:08:07 -0300

    feat: adicionado o tema escuro e troca de cores no botão

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,14 @@
 import { Button } from "@mui/material";
 import { Routes, Route, Navigate } from "react-router-dom";
+import { useAppThemeContext } from "../shared/contexts";
 
 export const AppRoutes = () => {
+    const { toggleTheme } = useAppThemeContext();
+
     return (
         <Routes>
             {/* Rota principal que renderiza o conteúdo da página inicial */}
-            <Route path="/pagina-inicial" element={<Button variant="contained" color="primary">Teste</Button>} /> 
+              <Route path="/pagina-inicial" element={<Button variant='contained' color='primary' onClick={toggleTheme}>Toggle theme</Button>} />
 
             {/* Redireciona qualquer rota não reconhecida para a página inicial */}
             <Route path="*" element={<Navigate to="/pagina-inicial" />} />
---
+++ src/shared/contexts/ThemeContext.tsx
@@ -0,0 +1,44 @@
+import { createContext, useCallback, useContext, useMemo, useState } from "react";
+import { ThemeProvider } from "@mui/material";
+import { Box } from "@mui/system";
+import { DarkTheme, LigthTheme } from "../themes";
+
+interface IThemeContextData {
+    themeName: 'light' | 'dark';
+    toggleTheme: () => void;
+}
+
+interface IAppThemeProviderProps {
+    children : React.ReactNode
+}
+
+const ThemeContext = createContext({} as IThemeContextData);
+
+export const useAppThemeContext = () => {
+    return useContext(ThemeContext)
+}
+
+export const AppThemeProvider: React.FC <IAppThemeProviderProps> = ( {children} ) => {
+    const [themeName, SetThemeName] = useState<'light' | 'dark'> ('light');
+
+    const toggleTheme = useCallback(() =>{
+        SetThemeName(oldThemeName => oldThemeName === 'light' ? 'dark' : 'light')
+    }, [])
+
+    const theme = useMemo(() => {
+        if (themeName === 'light') return LigthTheme
+
+        return DarkTheme
+    }, [themeName])
+
+    return(
+       <ThemeContext.Provider value = {{ themeName, toggleTheme}}>
+        <ThemeProvider theme={theme}>
+            <Box width="100vw" height="100vh" bgcolor={theme.palette.background.default} >
+            {children}
+            </Box>
+        </ThemeProvider>
+       </ThemeContext.Provider> 
+    )
+}
+
